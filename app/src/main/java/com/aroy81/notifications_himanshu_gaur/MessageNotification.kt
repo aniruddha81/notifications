ca13d29f2ca392030e4de4a8220818da8683f7e8
Aniruddha Roy <ar.roy564@gmail.com>
2025-03-25 01:49:29 +0600

    message notification

---
+++ app/src/main/java/com/aroy81/notifications_himanshu_gaur/MessageNotification.kt
@@ -0,0 +1,107 @@
+package com.aroy81.notifications_himanshu_gaur
+
+import android.Manifest.permission.POST_NOTIFICATIONS
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.app.Person
+import androidx.core.app.RemoteInput
+import androidx.core.content.ContextCompat
+
+const val REPLY = "reply"
+
+fun message(context: Context) {
+    val replyInput = RemoteInput.Builder(REPLY).setLabel("reply").build()
+
+    val intent = Intent(context, MessageReceiver::class.java)
+
+    val pendingIntent = PendingIntent.getBroadcast(
+        context,
+        1,
+        intent,
+        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
+    )
+
+    val action =
+        NotificationCompat.Action.Builder(R.drawable.ic_launcher_foreground, "Reply", pendingIntent)
+            .addRemoteInput(replyInput)
+
+    val person = Person.Builder().setName("Aniruddha").build()
+    val style =
+        NotificationCompat.MessagingStyle(person).setConversationTitle("conversation").addMessage(
+            "text 1",
+            System.currentTimeMillis(), person
+        ).addMessage("text 2", System.currentTimeMillis(), person)
+
+    val notification = NotificationCompat.Builder(context, DEFAULT_ID)
+        .setSmallIcon(R.drawable.ic_launcher_background).setContentTitle("title")
+        .setContentText("description").setStyle(style).addAction(action.build()).build()
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        if (ContextCompat.checkSelfPermission(
+                context,
+                POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            NotificationManagerCompat.from(context).notify(1, notification)
+        }
+    } else {
+        NotificationManagerCompat.from(context).notify(1, notification)
+    }
+}
+
+class MessageReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        RemoteInput.getResultsFromIntent(intent!!)?.getCharSequence(REPLY).let { text ->
+            val replyInput = RemoteInput.Builder(REPLY).setLabel("reply").build()
+
+            val replyIntent = Intent(context, MessageReceiver::class.java)
+
+            val pendingIntent = PendingIntent.getBroadcast(
+                context,
+                1,
+                replyIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
+            )
+
+            val action =
+                NotificationCompat.Action.Builder(
+                    R.drawable.ic_launcher_foreground,
+                    "Reply",
+                    pendingIntent
+                )
+                    .addRemoteInput(replyInput)
+
+            val person = Person.Builder().setName("Aniruddha").build()
+            val person2 = Person.Builder().setName("VOdaaaa").build()
+            val style =
+                NotificationCompat.MessagingStyle(person).setConversationTitle("conversation")
+                    .addMessage(
+                        "text 1",
+                        System.currentTimeMillis(), person
+                    ).addMessage("text 2", System.currentTimeMillis(), person)
+                    .addMessage(text, System.currentTimeMillis(), person2)
+
+            val notification = NotificationCompat.Builder(context!!, DEFAULT_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background).setContentTitle("title")
+                .setContentText("description").setStyle(style).addAction(action.build()).build()
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                if (ContextCompat.checkSelfPermission(
+                        context,
+                        POST_NOTIFICATIONS
+                    ) == PackageManager.PERMISSION_GRANTED
+                ) {
+                    NotificationManagerCompat.from(context).notify(1, notification)
+                }
+            } else {
+                NotificationManagerCompat.from(context).notify(1, notification)
+            }
+        }
+    }
+}
